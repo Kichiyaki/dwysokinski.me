3e71ccaa62618f67223296f16354db59828a3811
Kichiyaki <xyztojajestem@gmail.com>
2021-10-18 07:58:05 +0200

    update caniuse-lite

--- src/libs/useSmoothScroll.js
+++ src/libs/useSmoothScroll.js
@@ -2,14 +2,16 @@ import isSmoothScrollSupported from '@utils/isSmoothScrollSupported';
 
 const useSmoothScroll = () => {
   return id => e => {
-    if (isSmoothScrollSupported()) {
-      e.preventDefault();
-      document.querySelector('#' + id).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-        inline: 'nearest',
-      });
+    if (!isSmoothScrollSupported()) {
+      return;
     }
+
+    e.preventDefault();
+    document.querySelector('#' + id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
   };
 };
 
